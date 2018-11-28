773d1864968eec07f3bebe9f531dc1f7bf3180df
thiyaguleet <44983163+thiyaguleet@users.noreply.github.com>
2018-11-27 23:44:57 -0800

    Rename Two Sum II - Input array is sorted.cpp to Two Sum II.cpp

---
+++ Two Sum II.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l=0, h=numbers.size()-1; 
+        while(l<h){
+            if(numbers[l]+numbers[h]==target) return vector<int> {l+1, h+1};
+            else if(numbers[l]+numbers[h]>target)
+                h--;
+            else l++;
+        }
+    }
+};
