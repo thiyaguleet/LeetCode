9125d271dd295a41d680b1351eccd0a50020b330
thiyaguleet <44983163+thiyaguleet@users.noreply.github.com>
2018-12-28 19:22:16 -0800

    Create Implement Stack using Queues.cpp

---
+++ Implement Stack using Queues.cpp
@@ -0,0 +1,51 @@
+class MyStack {
+public:
+    /** Initialize your data structure here. */
+    queue<int> q;
+    MyStack() {
+        
+    }
+    /** Push element x onto stack. */
+    void push(int x) {
+        q.push(x);
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        int size=q.size();
+        for(int i=0; i<size-1; i++){
+            q.push(q.front());
+            q.pop();
+        }
+        int val=q.front();
+        q.pop();
+        return val;
+    }
+    
+    /** Get the top element. */
+    int top() {
+        int size=q.size();
+        for(int i=0; i<size-1; i++){
+            q.push(q.front());
+            q.pop();
+        }
+        int val=q.front();
+        q.push(q.front());
+        q.pop();
+        return val;
+    }
+    
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        return q.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * bool param_4 = obj.empty();
+ */
